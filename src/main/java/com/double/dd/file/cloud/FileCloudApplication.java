b8c24da5b12072735466d71947bea6c7a13e4d12
kedong <1150744690@qq.com>
2019-09-28 10:36:38 +0800

    init

---
+++ src/main/java/com/double/dd/file/cloud/FileCloudApplication.java
@@ -0,0 +1,13 @@
+package com.double.dd.file.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCloudApplication.class, args);
+	}
+
+}
